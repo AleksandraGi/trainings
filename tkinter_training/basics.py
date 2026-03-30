16b7056bb6c3c86082d5c92fbb014674720b4bfd
AleksandraGi <aleksandra.m.giemza@gmail.com>
2026-03-30 16:12:58 +0200

    Window, Label, Button

--- tkinter_training/basics.py
+++ tkinter_training/basics.py
@@ -1,17 +1,81 @@
 # https://www.youtube.com/watch?v=lyoyTlltFVU&list=PLZPZq0r_RZOOeQBaP5SeMjl2nwDcJaV0T
 
 from tkinter import *
+from PIL import Image, ImageTk
 
-BACKGROUND_COLOUR = "#484848"
-# BACKGROUND_COLOUR = '#FFFFFF'
 
-window = Tk()
-window.geometry("400x300")
-window.title("GUI")
+BACKGROUND_COLOUR = "#FFFFFF"
+TEXT_COLOUR = "#6A1411"
+BUTTON_COLOUR = "#522990"
+BUTTON_TEXT_COLOUR = "#FFFFF9"
+BUTTON_ACTIVE_BACK_COLOUR = "#ED10ED"
+BUTTON_ACTIVE_FRONT_COLOUR = "#000000"
 
-icon = PhotoImage(file='tkinter_training\images\cat.png')
-window.iconphoto(True, icon)
 
-window.config(background=BACKGROUND_COLOUR)
+# ------- WINDOW ------- (serves as a container to keep widgets)
+window = Tk()                                               # instantiate an instance of a window
+window.geometry("500x600")                                  # size of the window
+window.title("GUI")                                         # window's title
 
-window.mainloop()
+icon = PhotoImage(file='tkinter_training\images\cat.png')   # path to window's icon
+window.iconphoto(True, icon)                                # window's icon
+
+window.config(background=BACKGROUND_COLOUR)                 # background colour
+
+
+# ------- LABELS ------- (an area widget that hold texts and/or an image within a window)   https://www.tutorialspoint.com/python/tk_label.htm
+photo = Image.open('tkinter_training\\images\\cat_drawing.jpg')
+photo = photo.resize((125,125))
+photo = ImageTk.PhotoImage(photo)
+
+label = Label(window, 
+              text="Hello", 
+              font=("Arial", 40,'bold'), 
+              bg=BACKGROUND_COLOUR, 
+              fg=TEXT_COLOUR,
+              relief=RAISED,                                # border style
+              bd=10,                                        # size of the border
+              padx=20,                                      # space between text and border (horizontal padding)
+              pady=20,                                      # vertical padding
+              image=photo,
+              compound="bottom"
+)
+
+# label.place(x=0, y=0)                                     # place the label in a specific place
+label.pack()
+
+
+# ------- BUTTON ------- (click it and sth happens)
+count = 0
+def click():
+    global count
+    count += 1
+    label2.config(text=f"Counter: {count}")
+    label3.pack()
+
+button = Button(window, text="Click me")
+button.config(command=click)                                # performs call back of function
+button.config(font=('Ink Free', 25, 'italic'))
+button.config(bg=BUTTON_COLOUR, fg=BUTTON_TEXT_COLOUR)
+button.config(activebackground=BUTTON_ACTIVE_BACK_COLOUR, activeforeground=BUTTON_ACTIVE_FRONT_COLOUR)
+
+button_icon = Image.open("tkinter_training\\images\\arrow-with-scribble.png")
+button_icon = button_icon.resize((50,50))
+button_icon = ImageTk.PhotoImage(button_icon)
+button.config(image=button_icon)
+button.config(compound="top")
+
+# button.config(state=DISABLED)                               # disables button (DISABLED or ACTIVE)
+
+label2 = Label(window, text="Counter", font=("Monospace", 15), bg=BACKGROUND_COLOUR)
+label2.pack()
+
+label3 = Label(window, image=button_icon)
+
+button.pack()
+
+
+# ------- USER INPUT -------
+
+
+window.mainloop()                                           # place window on a screen
--- tkinter_training/info.md
+++ tkinter_training/info.md
@@ -2,3 +2,6 @@ Icon source:
 https://www.flaticon.com/packs/hand-drawn-arrows
 https://www.flaticon.com/packs/flowers-59
 https://www.flaticon.com/packs/origami-24
+
+Photo source:
+https://www.flickr.com/photos/vintage_illustration/32200176938/in/gallery-141423754@N05-72157714380642337/
