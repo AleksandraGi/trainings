140be5355f23035a14f26fe506304c5a875ed8ed
AleksandraGi <aleksandra.m.giemza@gmail.com>
2026-03-31 16:47:31 +0200

    User input and checkboxes

--- tkinter_training/basics.py
+++ tkinter_training/basics.py
@@ -12,15 +12,17 @@ BUTTON_ACTIVE_BACK_COLOUR = "#ED10ED"
 BUTTON_ACTIVE_FRONT_COLOUR = "#000000"
 
 
+
 # ------- WINDOW ------- (serves as a container to keep widgets)
 window = Tk()                                               # instantiate an instance of a window
 window.geometry("500x600")                                  # size of the window
 window.title("GUI")                                         # window's title
 
-icon = PhotoImage(file='tkinter_training\images\cat.png')   # path to window's icon
+icon = PhotoImage(file='images\\cat.png')   # path to window's icon
 window.iconphoto(True, icon)                                # window's icon
 
 window.config(background=BACKGROUND_COLOUR)                 # background colour
+
 
 
 # # ------- LABELS ------- (an area widget that hold texts and/or an image within a window)   https://www.tutorialspoint.com/python/tk_label.htm
@@ -43,6 +45,7 @@ window.config(background=BACKGROUND_COLOUR)                 # background colour
 
 # # label.place(x=0, y=0)                                     # place the label in a specific place
 # label.pack()
+
 
 
 # # ------- BUTTON ------- (click it and sth happens)
@@ -75,43 +78,114 @@ window.config(background=BACKGROUND_COLOUR)                 # background colour
 # button.pack()
 
 
-# ------- USER INPUT -------
-def submit():
-    username = entry.get()
-    print(username)
 
-def delete():
-    entry.delete(0, END)
+# # ------- USER INPUT -------
+# def submit():   
+#     username = entry.get()                              # saves input into a variable
+#     print(username)
+
+# def delete():
+#     entry.delete(0, END)                                # deletes the whole line of text
+
+# def backspace():
+#     entry.delete(len(entry.get())-1, END)               # deletes last character
+
+# submit_button = Button(window, text="Submit", command=submit)
+# submit_button.pack(side="left")
+
+# delete_button = Button(window, text="Delete", command=delete)
+# delete_button.pack(side="right")
+
+# backspace = Button(window, text="Backspace", command=backspace)
+# backspace.pack(side="right")
+
+# def limit_size(new_text):
+#     return len(new_text) <= 10
+
+# entry = Entry(window,
+#     font=("Times New Roman", 50),
+    
+#     bg="pink",
+#     fg="dark blue",
+#     width=12,
+#     show="*",                                               # replaces everything we write into *
+#     validate="key"
+# )
+# # entry.insert(0, "teXt")                                   # text visible when you open the app
+# # entry.config(state=DISABLED)                              # ACTIVE or DISABLED
+
+# vcmd = (window.register(limit_size), "%P")
+# entry.config(validatecommand=vcmd)
+
+# entry.pack()
 
 
 
-submit_button = Button(window, text="Submit", command=submit)
-submit_button.pack(side="left")
+# ------- CHECKBUTTONS -------
+x = IntVar()
+y = IntVar()
 
-delete_button = Button(window, text="Delete", command=delete)
-delete_button.pack(side="right")
+def display():
+    if x.get() == 1 and y.get() == 0:
+        print("You chose red")
+    elif x.get() == 0 and y.get() == 1:
+        print("You chose pink")
+    elif x.get() == 0 and y.get() == 0:
+        print("You didn't choose your colour")
+    else:
+        print("You chose both colours")
 
+red_photo = PhotoImage(file='images\\red.png')
+pink_photo = PhotoImage(file='images\\pink.png')
 
-def limit_size(new_text):
-    return len(new_text) <= 10
+checkbox_red = Checkbutton(
+    window, 
+    text="Red", 
+    bg=BACKGROUND_COLOUR,
+    fg="red",
+    variable=x,
+    onvalue=1,                              # what x is equal to when the button is checked
+    offvalue=0,                             # what x is equal to when the button is unchecked
+    command=display
+    )
 
-entry = Entry(window,
-    font=("Times New Roman", 50),
-    bg="pink",
-    fg="dark blue",
-    width=12,
-    show="*",                                               # replaces everything we write into *
-    validate="key"
+checkbox_red.config(
+    font=("Arial", 20),
+    activeforeground="red"
+    )
+
+checkbox_red.config(
+    image=red_photo,
+    compound="left"
 )
-# entry.insert(0, "teXt")                                   # text visible when you open the app
-# entry.config(state=DISABLED)                              # ACTIVE or DISABLED
-
-vcmd = (window.register(limit_size), "%P")
-entry.config(validatecommand=vcmd)
+checkbox_red.config(padx=25, pady=10, width=250, height=50)
+checkbox_red.config(anchor="w")
+checkbox_red.pack()
 
 
+checkbox_pink = Checkbutton(
+    window, text="Pink", 
+    bg=BACKGROUND_COLOUR,
+    fg="pink",
+    variable=y,
+    onvalue=1,                              # what x is equal to when the button is checked
+    offvalue=0,                             # what x is equal to when the button is unchecked
+    command=display
+    )
+checkbox_pink.config(
+    font=("Arial", 20),
+    activeforeground="pink"
+    )
 
-entry.pack()
+checkbox_pink.config(
+    image=pink_photo,
+    compound="left"
+)
+checkbox_pink.config(padx=25, pady=10, width=250, height=50)
+checkbox_pink.config(anchor="w")
+
+
+checkbox_pink.pack()
 
 
 
