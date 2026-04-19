1ff2de661eaad09ea0402d9627558b7a102e963f
AleksandraGi <aleksandra.m.giemza@gmail.com>
2026-04-19 21:34:55 +0200

    Radiobuttons

--- tkinter_training/basics.py
+++ tkinter_training/basics.py
@@ -15,16 +15,16 @@ BUTTON_ACTIVE_FRONT_COLOUR = "#000000"
 
 # ------- WINDOW ------- (serves as a container to keep widgets)
 window = Tk()                                               # instantiate an instance of a window
-window.geometry("500x600")                                  # size of the window
+# window.geometry("500x600")                                  # size of the window
 window.title("GUI")                                         # window's title
 
-icon = PhotoImage(file='images\\cat.png')   # path to window's icon
+icon = PhotoImage(file='images\\cat.png')                   # path to window's icon
 window.iconphoto(True, icon)                                # window's icon
 
 window.config(background=BACKGROUND_COLOUR)                 # background colour
 
-
-
+# labels
+"""
 # # ------- LABELS ------- (an area widget that hold texts and/or an image within a window)   https://www.tutorialspoint.com/python/tk_label.htm
 # photo = Image.open('tkinter_training\\images\\cat_drawing.jpg')
 # photo = photo.resize((125,125))
@@ -45,9 +45,10 @@ window.config(background=BACKGROUND_COLOUR)                 # background colour
 
 # # label.place(x=0, y=0)                                     # place the label in a specific place
 # label.pack()
+"""
 
-
-
+#buttons
+"""
 # # ------- BUTTON ------- (click it and sth happens)
 # count = 0
 # def click():
@@ -77,8 +78,10 @@ window.config(background=BACKGROUND_COLOUR)                 # background colour
 
 # button.pack()
 
+"""
 
-
+# user input
+"""
 # # ------- USER INPUT -------
 # def submit():   
 #     username = entry.get()                              # saves input into a variable
@@ -119,74 +122,129 @@ window.config(background=BACKGROUND_COLOUR)                 # background colour
 
 # entry.pack()
 
+"""
+
+#checkbuttons
+"""
+# # ------- CHECKBUTTONS -------
+# x = IntVar()
+# y = IntVar()
+
+# def display():
+#     if x.get() == 1 and y.get() == 0:
+#         print("You chose red")
+#     elif x.get() == 0 and y.get() == 1:
+#         print("You chose pink")
+#     elif x.get() == 0 and y.get() == 0:
+#         print("You didn't choose your colour")
+#     else:
+#         print("You chose both colours")
+
+# red_photo = PhotoImage(file='images\\red.png')
+# pink_photo = PhotoImage(file='images\\pink.png')
+
+# checkbox_red = Checkbutton(
+#     window, 
+#     text="Red", 
+#     bg=BACKGROUND_COLOUR,
+#     fg="red",
+#     variable=x,
+#     onvalue=1,                              # what x is equal to when the button is checked
+#     offvalue=0,                             # what x is equal to when the button is unchecked
+#     command=display
+#     )
+
+# checkbox_red.config(
+#     font=("Arial", 20),
+#     activeforeground="red"
+#     )
+
+# checkbox_red.config(
+#     image=red_photo,
+#     compound="left"
+# )
+# checkbox_red.config(padx=25, pady=10, width=250, height=50)
+# checkbox_red.config(anchor="w")
+# checkbox_red.pack()
 
 
-# ------- CHECKBUTTONS -------
-x = IntVar()
-y = IntVar()
+# checkbox_pink = Checkbutton(
+#     window, text="Pink", 
+#     bg=BACKGROUND_COLOUR,
+#     fg="pink",
+#     variable=y,
+#     onvalue=1,                              # what x is equal to when the button is checked
+#     offvalue=0,                             # what x is equal to when the button is unchecked
+#     command=display
+#     )
+# checkbox_pink.config(
+#     font=("Arial", 20),
+#     activeforeground="pink"
+#     )
 
-def display():
-    if x.get() == 1 and y.get() == 0:
-        print("You chose red")
-    elif x.get() == 0 and y.get() == 1:
-        print("You chose pink")
-    elif x.get() == 0 and y.get() == 0:
-        print("You didn't choose your colour")
+# checkbox_pink.config(
+#     image=pink_photo,
+#     compound="left"
+# )
+# checkbox_pink.config(padx=25, pady=10, width=250, height=50)
+# checkbox_pink.config(anchor="w")
+
+
+# checkbox_pink.pack()
+"""
+
+# radiobuttons
+"""
+# ------- RADIOBUTTONS ------- (you can select only one from a group)
+food = ["pizza", "hamburger", "hotdog"]
+
+def order():
+    if x.get() == 0:
+        print("You ordered pizza")
+    elif x.get() == 1:
+        print("You ordered a burger")
+    elif x.get() == 2:
+        print("You ordered a hot dog")
     else:
-        print("You chose both colours")
+        print("nah")
 
-red_photo = PhotoImage(file='images\\red.png')
-pink_photo = PhotoImage(file='images\\pink.png')
+x = IntVar()
 
-checkbox_red = Checkbutton(
-    window, 
-    text="Red", 
-    bg=BACKGROUND_COLOUR,
-    fg="red",
-    variable=x,
-    onvalue=1,                              # what x is equal to when the button is checked
-    offvalue=0,                             # what x is equal to when the button is unchecked
-    command=display
+photo_pizza = Image.open('images\\pizza.png')
+photo_pizza = photo_pizza.resize((125,125))
+photo_pizza = ImageTk.PhotoImage(photo_pizza)
+
+photo_burger = Image.open('images\\burger.png')
+photo_burger = photo_burger.resize((125,125))
+photo_burger = ImageTk.PhotoImage(photo_burger)
+
+photo_hotdog = Image.open('images\\hot-dog.png')
+photo_hotdog = photo_hotdog.resize((125,125))
+photo_hotdog = ImageTk.PhotoImage(photo_hotdog)
+
+
+# photo_pizza = PhotoImage(file='images\\pizza.png')
+# photo_burger = PhotoImage(file='images\\burger.png')
+# photo_hotdog = PhotoImage(file='images\\hot-dog.png')
+food_images = [photo_pizza, photo_burger, photo_hotdog]
+
+for index in range(len(food)):
+    radiobutton = Radiobutton(window,       
+                              text=food[index],         # adds text to radiobutttons
+                              variable=x,               # groups radiobuttons together if thety share the same variable
+                              value=index,              # assigns each radiobutton a different value
+                              padx=25,                  # adds padding on x-axis
+                              font=("Impact", 50),
+                              image=food_images[index], # adds image to radiobutton
+                              compound="left",          # adds image and text (left side)
+                              indicator=0,              # eliminates circle indicators
+                              width=500,                # sets width of buttons
+                              command=order             # set command of buttons to function
     )
+    # print(x, index)           # PY_VAR0 0     PY_VAR0 1   PY_VAR0 2
 
-checkbox_red.config(
-    font=("Arial", 20),
-    activeforeground="red"
-    )
-
-checkbox_red.config(
-    image=red_photo,
-    compound="left"
-)
-checkbox_red.config(padx=25, pady=10, width=250, height=50)
-checkbox_red.config(anchor="w")
-checkbox_red.pack()
-
-
-checkbox_pink = Checkbutton(
-    window, text="Pink", 
-    bg=BACKGROUND_COLOUR,
-    fg="pink",
-    variable=y,
-    onvalue=1,                              # what x is equal to when the button is checked
-    offvalue=0,                             # what x is equal to when the button is unchecked
-    command=display
-    )
-checkbox_pink.config(
-    font=("Arial", 20),
-    activeforeground="pink"
-    )
-
-checkbox_pink.config(
-    image=pink_photo,
-    compound="left"
-)
-checkbox_pink.config(padx=25, pady=10, width=250, height=50)
-checkbox_pink.config(anchor="w")
-
-
-checkbox_pink.pack()
-
+    radiobutton.pack(anchor=W)
+"""
 
 
 
--- tkinter_training/info.md
+++ tkinter_training/info.md
@@ -1,7 +1,11 @@
 Icon sources:
-https://www.flaticon.com/packs/hand-drawn-arrows
-https://www.flaticon.com/packs/flowers-59
-https://www.flaticon.com/packs/origami-24
+arrow: https://www.flaticon.com/packs/hand-drawn-arrows
+flower: https://www.flaticon.com/packs/flowers-59
+origami set: https://www.flaticon.com/packs/origami-24
+hotdog: https://www.flaticon.com/free-icon/hot-dog_8932659
+burger: https://www.flaticon.com/free-icon/burger_8932653
+pizza: https://www.flaticon.com/free-icon/pizza_8932669
+
 
 Photo source:
 https://www.flickr.com/photos/vintage_illustration/32200176938/in/gallery-141423754@N05-72157714380642337/
