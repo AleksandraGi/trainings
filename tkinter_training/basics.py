bdc30c5655ec0db930af2fa514956377b6464e17
AleksandraGi <aleksandra.m.giemza@gmail.com>
2026-04-21 13:38:42 +0200

    scales

--- tkinter_training/basics.py
+++ tkinter_training/basics.py
@@ -246,6 +246,47 @@ for index in range(len(food)):
     radiobutton.pack(anchor=W)
 """
 
+# scale
+"""
+def submit():
+    print("The temperature: " + str(scale.get()) + " degrees C")
+
+fire_image = Image.open('images\\fire.png')
+fire_image = fire_image.resize((75,75))
+fire_image = ImageTk.PhotoImage(fire_image)
+fire_label = Label(image=fire_image)
+fire_label.pack()
 
 
-window.mainloop()                                           # place window on a screen
+scale = Scale(window, 
+              from_=100, 
+              to=0,
+              length=600,
+              orient=VERTICAL,          # orientation of scale
+              font=("Consolas", 20),
+              tickinterval=10,          # adds numeric indicators for value
+            #   showvalue=0,              # hide current value
+              resolution=5,             # increment of slider
+              troughcolor="#69EAFF",
+              fg="#FF1C00",
+              bg="#111111",
+
+              )
+scale.set(((scale['from']-scale['to']) / 2) + scale['to'])
+
+scale.pack()
+
+
+ice_image = Image.open('images\\snowflake.png')
+ice_image = ice_image.resize((75,75))
+ice_image = ImageTk.PhotoImage(ice_image)
+ice_label = Label(image=ice_image)
+ice_label.pack()
+
+button = Button(window, text="sumbit", command=submit)
+button.pack()
+"""
+
+
+
+window.mainloop()                                           # place window on a screen 
--- tkinter_training/info.md
+++ tkinter_training/info.md
@@ -5,7 +5,8 @@ origami set: https://www.flaticon.com/packs/origami-24
 hotdog: https://www.flaticon.com/free-icon/hot-dog_8932659
 burger: https://www.flaticon.com/free-icon/burger_8932653
 pizza: https://www.flaticon.com/free-icon/pizza_8932669
-
+fire: https://www.flaticon.com/free-icon/fire_1453025
+snowflake: https://www.flaticon.com/free-icon/snowflake_1453023
 
 Photo source:
 https://www.flickr.com/photos/vintage_illustration/32200176938/in/gallery-141423754@N05-72157714380642337/
